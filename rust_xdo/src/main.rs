e120813dc23e8baebefb5e4ffe9fee850d93c2ee
Frank Pereny <fjpereny@gmai.com>
2022-12-12 00:01:55 +0800

    Updates

--- rust_xdo/src/main.rs
+++ rust_xdo/src/main.rs
@@ -2,6 +2,13 @@
 mod rust_xdo;
 
 fn main() {
-    println!("Pressing 1 key");
-    rust_xdo::press("1"); 
+
+    let x = 50;
+    let x: String = x.to_string();
+    println!("{}", x);
+
+    // println!("Pressing 1 key");
+    // rust_xdo::press("1\n"); 
+    // rust_xdo::typewrite("Hello, world!".to_string(), 37);
+    // rust_xdo::get_active_window();
 }
--- rust_xdo/src/rust_xdo.rs
+++ rust_xdo/src/rust_xdo.rs
@@ -1,11 +1,38 @@
 
 use std::process::Command;
 
+// KEYBOARD INPUTS
 
+// KEYPRESS
 pub(crate) fn press(key: &str) {
-        Command::new("xdotool")
+    Command::new("xdotool")
         .arg("key")
         .arg(key)
+        .spawn()
+        .expect("Command failed to spawn");
+}
+
+// TYPEWRITE
+pub(crate) fn typewrite(text: String, delay: u32) {
+
+    let delay = delay.to_string();
+
+    Command::new("xdotool")
+        .arg("type")
+        .arg("--delay")
+        .arg(delay)
+        .arg(text)
+        .spawn()
+        .expect("Command failed to spawn");
+}
+
+
+// WINDOWS
+
+// GET NAME OF ACTIVE WINDOW
+pub(crate) fn get_active_window() -> &str {
+    Command::new("xdotool")
+        .arg("getactivewindow")
         .spawn()
         .expect("Command failed to spawn");
 }
